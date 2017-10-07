c6f315780e6ba84ef88809c6782f30acca18edbc
cvols <chris.volonnino@gmail.com>
2017-10-07 13:25:46 -0400

    boss changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    
+
     $('#submit').on('click', function(event) {
 
         event.preventDefault();
@@ -23,20 +25,14 @@ $(document).ready(function() {
             var coverArt = json.results[randomNumber].artworkUrl100;
             var audioFile = json.results[randomNumber].previewUrl;
             var songPrice = json.results[randomNumber].trackPrice;
+            if (songPrice == '-1'){
+                songPrice = 'Album Only';
+            }
             var albumPrice = json.results[randomNumber].collectionPrice;
 
-            $('#itunes').append(artistName);
-            $('#itunes').append(albumName);
-            $('#itunes').append(songName);
-            $('#itunes').append(songPrice);
-            $('#itunes').append(albumPrice);
-
-            $('#itunes').append('<audio controls autoplay> <source src="' + audioFile + '"type="audio/x-m4a"></audio>');
-
-            $('#itunes').append('<img src="' + coverArt + '"alt="' + songName + ' Picture"/>');
+            $('#itunes').append('<tr><td>' + artistName + '</td><td>' + songName + '</td><td><img src="' + coverArt + '"alt="' + songName + ' Picture"/><td>' + albumName + '</td><td>' + songPrice + '</td><td>' + albumPrice + '</td><td><audio controls> <source src="' + audioFile + '"type="audio/x-m4a"></audio></td></tr>');
 
             var weatherSearch = $('#location').val().trim();
-            weatherSearch = 'Hoboken,us';
 
             var weatherQueryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + weatherSearch + '&units=imperial&mode=json&APPID=7c13bf011272646fc4a945352ad1a721';
 
@@ -46,17 +42,12 @@ $(document).ready(function() {
             }).done(function(response) {
                 console.log(response);
                 var weatherIcon = response.weather[0].icon;
-                var weatherPic = '<img src="http://openweathermap.org/img/w/' + weatherIcon + '.png"/>';
-                $('#weather').append(weatherPic);
-
+                var weatherPic = '<img height="75px" width="75px" src="http://openweathermap.org/img/w/' + weatherIcon + '.png"/>';
                 var overCast = response.weather[0].description;
                 var temp = response.main.temp;
                 temp = Math.round(temp);
-                $('#weather').append(overCast);
-                $('#weather').append('<p>' + temp + '&#8457</p>');
 
-
-
+                $('#weather').html('<div class="col-md-6"><img height="100px" width="100px" src="http://openweathermap.org/img/w/' + weatherIcon + '.png"/></div><p class="weather-size"><strong>' + temp + '&#8457</strong></p><p class="weather-size">' + overCast + '</p></div>');
 
             }); // end itunes ajax call
 
